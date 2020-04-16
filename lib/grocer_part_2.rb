50faef31a8d8eccdabe52e781825c39f59af0561
Colton Kaiser <kaiserc2@seattleu.edu>
2020-04-16 03:39:29 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -41,7 +41,6 @@ def checkout(cart, coupons)
   #
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
-  binding.pry
   consolidate_cart(cart)
   apply_coupons(cart, coupons)
   apply_clearance(cart)
