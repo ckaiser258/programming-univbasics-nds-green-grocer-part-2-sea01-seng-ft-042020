e8b1120a58479d48dbcbc29279c0f3cb37908599
Colton Kaiser <kaiserc2@seattleu.edu>
2020-04-16 03:02:46 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,9 +1,29 @@
 require_relative './part_1_solution.rb'
+require "pry"
 
 def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  if coupons
+  cart.each do |item_info|
+    coupons.each do |coupon_info|
+    if coupon_info[:item] == item_info[:item]
+      item_info[:count] = item_info[:count] - coupon_info[:num]
+              cart << {
+              :item => coupon_info[:item].concat(" W/ COUPON"),
+              :price => coupon_info[:cost]/coupon_info[:num],
+              :clearance => item_info[:clearance], 
+              :count => coupon_info[:num]
+              } 
+    end
+    # if item_info[:count] < 1
+    #   cart.delete(item_info)
+    #end
+  end
+  end
+end
+cart
 end
 
 def apply_clearance(cart)
